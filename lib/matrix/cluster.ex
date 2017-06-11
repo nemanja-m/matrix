62e5b336d3c217f0a98677ab74e4bb2e80f811fa
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-11 02:54:10 +0200

    Register/Unregister nodes to/from cluster

--- lib/matrix/cluster.ex
+++ lib/matrix/cluster.ex
@@ -11,13 +11,43 @@ defmodule Matrix.Cluster do
     GenServer.call(__MODULE__, {:nodes})
   end
 
+  def register_node(aliaz: aliaz, address: address) do
+    GenServer.call(__MODULE__, {:register_node, aliaz, address})
+  end
+
+  def unregister_node(aliaz) do
+    GenServer.cast(__MODULE__, {:unregister_node, aliaz})
+  end
+
+  def clear do
+    GenServer.cast(__MODULE__, {:clear})
+  end
+
   def handle_call({:nodes}, _from, nodes) do
     {:reply, nodes_list(nodes), nodes}
   end
 
+  def handle_call({:register_node, aliaz, address}, _from, nodes) do
+    if Map.has_key?(nodes, aliaz) do
+      {:reply, :already_exist, nodes}
+    else
+      nodes = Map.put(nodes, aliaz, address)
+
+      {:reply, :ok, nodes}
+    end
+  end
+
+  def handle_cast({:unregister_node, aliaz}, nodes) do
+    {:noreply, Map.delete(nodes, aliaz)}
+  end
+
+  def handle_cast({:clear}, _nodes) do
+    {:noreply, this_node()}
+  end
+
   def init(_) do
     # Initialize cluster with this node
-    {:ok, Map.put(%{}, Configuration.this_aliaz, Configuration.this_address)}
+    {:ok, this_node()}
   end
 
   defp nodes_list(nodes) do
@@ -25,5 +55,9 @@ defmodule Matrix.Cluster do
     |> Enum.map(fn {aliaz, address} ->
       %AgentCenter{aliaz: aliaz, address: address}
     end)
+  end
+
+  defp this_node do
+    Map.put(%{}, Configuration.this_aliaz, Configuration.this_address)
   end
 end
--- test/lib/matrix/cluster_test.exs
+++ test/lib/matrix/cluster_test.exs
@@ -1,7 +1,11 @@
 defmodule Matrix.ClusterTest do
   use ExSpec, async: true
 
-  alias Matrix.{Cluster, Configuration}
+  alias Matrix.{Cluster, Configuration, AgentCenter}
+
+  setup do
+    Cluster.clear
+  end
 
   describe ".start_link" do
     it "initializes cluster with this node" do
@@ -9,4 +13,48 @@ defmodule Matrix.ClusterTest do
     end
   end
 
+  describe ".nodes" do
+    it "returns nodes in cluster" do
+      assert Cluster.nodes == [Configuration.this]
+    end
+  end
+
+  describe ".clear" do
+    it "deletes all nodes except this" do
+      :ok = Cluster.register_node(aliaz: "Neptune", address: "MilkyWay")
+      assert Cluster.nodes == [Configuration.this, %AgentCenter{aliaz: "Neptune", address: "MilkyWay"}]
+
+      Cluster.clear
+      assert Cluster.nodes == [Configuration.this]
+    end
+  end
+
+  describe ".register_node" do
+    context "when node doesn't exist" do
+      it "adds node to cluster" do
+        assert Cluster.register_node(aliaz: "Neptune", address: "MilkyWay") == :ok
+        assert Cluster.nodes == [Configuration.this, %AgentCenter{aliaz: "Neptune", address: "MilkyWay"}]
+      end
+    end
+
+    context "when node already exist" do
+      it "doesn't add node to cluster" do
+        node = [
+          {:aliaz, Configuration.this_aliaz},
+          {:address, Configuration.this_address}
+        ]
+
+        assert Cluster.register_node(node) == :already_exist
+        assert Cluster.nodes == [Configuration.this]
+      end
+    end
+  end
+
+  describe ".unregister_node" do
+    it "deletes node from cluster" do
+      Cluster.unregister_node(Configuration.this_aliaz)
+
+      assert Cluster.nodes == []
+    end
+  end
 end
