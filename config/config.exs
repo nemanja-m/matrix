125aa848e0425bbc113dfa8467680567f60f95f4
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-16 22:10:22 +0200

    Set agents directory from env variable

--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,7 @@ config :logger, :console,
 config :matrix, :master_node, System.get_env["MASTER"]
 config :matrix, :master_node_url, System.get_env["MASTER_URL"]
 config :matrix, :aliaz, System.get_env["ALIAS"]
+config :matrix, :agents_dir, System.get_env["AGENTS_DIR"]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
