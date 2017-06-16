17f6864e1c010e010ebb29dcf1b7395a7de93c98
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-16 05:05:42 +0200

    Implement abstract agent behaviour

--- lib/matrix/structs/agent.ex
+++ lib/matrix/structs/agent.ex
@@ -3,16 +3,87 @@ defmodule Matrix.Agent do
   Represents agent.
   """
 
+  alias Matrix.{Agent, AID, AgentType, AgentCenter, Configuration}
+
   @derive [Poison.Encoder]
   defstruct [:id]
 
-  @type t :: %__MODULE__{id: Matrix.AID.t}
+  @type t :: %__MODULE__{id: AID.t}
 
-  alias Matrix.{Agent, AID, AgentType, AgentCenter}
+  @callback handle_message(message :: AclMessage.t, state :: any)
+    :: {:ok, any, any} | {:error, any, any}
+
+  defmacro __using__(options) do
+    quote do
+      use GenServer
+
+      @behaviour Matrix.Agent
+
+      def start_link(args, _options \\ []) do
+        name = elem(args, 1) |> String.capitalize |> String.to_atom
+
+        GenServer.start_link(__MODULE__, name, name: name)
+      end
+
+      def init(name) do
+        state =
+          Matrix.Agent.new(name, type())
+          |> Map.merge(unquote(options[:state]))
+
+        {:ok, state}
+      end
+
+      def handle_call({:handle_message, message}, _form, state) do
+        case __MODULE__.handle_message(message, state) do
+          {:ok, result, new_state}    -> {:reply, {:ok, result}, new_state}
+          {:error, reason, new_state} -> {:reply, {:error, reason}, new_state}
+        end
+      end
+
+      def handle_call(:state, _from, state) do
+        {:reply, state, state}
+      end
+
+      defp type do
+        name =
+          __MODULE__
+          |> Module.split
+          |> List.last
+
+        module =
+          __MODULE__
+          |> Module.split
+          |> List.delete(name)
+          |> Enum.join(".")
+
+        %Matrix.AgentType{
+          name: name,
+          module: module
+        }
+      end
+    end
+  end
+
+  @spec new(name :: String.t, type :: AgentType.t) :: Agent.t
+  def new(name, type) do
+    %Agent{
+      id: %AID{
+        name: name,
+        host: Configuration.this,
+        type: type
+      }
+    }
+  end
 
   def from_hash(hash) do
     hash
     |> Poison.encode!
-    |> Poison.decode!(as: %Agent{id: %AID{host: %AgentCenter{}, type: %AgentType{}}})
+    |> Poison.decode!(as: %Agent{
+      id: %AID{
+        host: %AgentCenter{},
+        type: %AgentType{}
+      }
+    })
   end
+
 end
