54602943f9bf25807db08d2e892f93f1ae68424b
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-20 03:57:22 +0200

    Create agent type actions module

---
+++ web/static/js/actions/types.js
@@ -0,0 +1,18 @@
+import api from '../api';
+
+export function getAgentTypes {
+  return (dispatch) => {
+    api
+      .getAgentTypes()
+      .then( response => {
+
+        // Parse response and form list of available agent types.
+        const agentTypes = Object
+          .entries(response.data.data)
+          .map( ([host, types]) => types)
+          .reduce( (acc, types) => [...acc, ...types] );
+
+        dispatch({ type: 'ADD_TYPES', types: agentTypes });
+      });
+  };
+}
