18f656ef626fd5b6ee4d1274014d173ca1950537
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-23 03:14:16 +0200

    Implement contract net protocol example

---
+++ agents/contract_net/initiator.ex
@@ -0,0 +1,46 @@
+defmodule ContractNet.Initiator do
+  use Matrix.Agent,
+    state: %{}
+
+  require Logger
+
+  alias Matrix.{AclMessage, MessageDispatcher, Agents}
+
+  def handle_message(message = %AclMessage{performative: :refuse}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    {:ok, state}
+  end
+
+  def handle_message(message = %AclMessage{performative: :propose}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    # Simulate work
+    :timer.sleep(:rand.uniform(5) * 1000)
+
+    receiver = Agents.find_by_name(message.sender)
+
+    # Accept proposal with 42% chance
+    performative = if :rand.uniform(100) <= 42, do: :accept_proposal, else: :reject_proposal
+
+    message = %AclMessage{performative: performative, sender: state.id.name}
+
+    MessageDispatcher.send_message(receiver, message)
+
+    {:ok, state}
+  end
+
+  def handle_message(message = %AclMessage{performative: :inform}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+    Logger.warn "Content: #{message.content}"
+
+    {:ok, state}
+  end
+
+  def handle_message(message = %AclMessage{performative: :failure}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    {:ok, state}
+  end
+
+end
---
+++ agents/contract_net/participant.ex
@@ -0,0 +1,50 @@
+defmodule ContractNet.Participant do
+  use Matrix.Agent,
+    state: %{}
+
+  require Logger
+
+  alias Matrix.{AclMessage, MessageDispatcher, Agents}
+
+  def handle_message(message = %AclMessage{performative: :cfp}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    # Simulate work
+    :timer.sleep(:rand.uniform(5) * 1000)
+
+    receiver = Agents.find_by_name(message.sender)
+
+    # Propose with 42% chance
+    performative = if :rand.uniform(100) <= 42, do: :propose, else: :refuse
+    message = %AclMessage{performative: performative, sender: state.id.name}
+
+    MessageDispatcher.send_message(receiver, message)
+
+    {:ok, state}
+  end
+
+  def handle_message(message = %AclMessage{performative: :accept_proposal}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    receiver = Agents.find_by_name(message.sender)
+
+    # Simulate work
+    :timer.sleep(:rand.uniform(5) * 1000)
+
+    performative = if :rand.uniform(100) <= 80, do: :inform, else: :failure
+    content = if performative == :inform, do: "Done"
+
+    message = %AclMessage{performative: performative, sender: state.id.name, content: content}
+
+    MessageDispatcher.send_message(receiver, message)
+
+    {:ok, state}
+  end
+
+  def handle_message(message = %AclMessage{performative: :reject_proposal}, state) do
+    Logger.warn "#{state.id.name} received #{message.performative} from #{message.sender}"
+
+    {:ok, state}
+  end
+
+end
